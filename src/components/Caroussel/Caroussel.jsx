0e92c05cf1dab440c79a2de639c6d3cf8f51c5a3
shaynlink <guemmoudrayane03@gmail.com>
2023-08-01 16:46:18 +0200

    fix: carrousel responsive + bug + number slide count

--- src/components/Caroussel/Caroussel.jsx
+++ src/components/Caroussel/Caroussel.jsx
@@ -15,11 +15,11 @@ export default function Caroussel({ images, alt = '' }) {
 
     function handleChangeIndex(direction) {
         if (direction === 'left') {
-            if (index === 0) setIndex(isMultiple - 1);
-            else setIndex(index - 1);
+            if (index === 0) return void setIndex(images.length - 1);
+            else return void setIndex(index - 1);
         } else {
-            if (index === isMultiple - 1) setIndex(0);
-            else setIndex(index + 1);
+            if (index === images.length - 1) return void setIndex(0);
+            else return void setIndex(index + 1);
         }
     }
 
@@ -28,13 +28,21 @@ export default function Caroussel({ images, alt = '' }) {
             <div className="caroussel">
                 {isMultiple && (
                     <div className="direction">
-                        <svg onClick={() => handleChangeIndex('left')} width="96" height="120" viewBox="0 0 96 120" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M70.04 15.7831L62.92 8.70312L23.36 48.3031L62.96 87.9031L70.04 80.8231L37.52 48.3031L70.04 15.7831Z" fill="white"/>
-                        </svg>
+                        <div className="arrow-container">
+                            <svg onClick={() => handleChangeIndex('left')} width="96" height="120" viewBox="0 0 96 120" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M70.04 15.7831L62.92 8.70312L23.36 48.3031L62.96 87.9031L70.04 80.8231L37.52 48.3031L70.04 15.7831Z" fill="white"/>
+                            </svg>
+                        </div>
 
-                        <svg onClick={() => handleChangeIndex('right')} width="96" height="121" viewBox="0 0 96 121" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M25.96 81.3458L33.04 88.4258L72.64 48.8258L33.04 9.22583L25.96 16.3058L58.48 48.8258L25.96 81.3458Z" fill="white"/>
-                        </svg>
+                        <div className="arrow-container">
+                            <svg onClick={() => handleChangeIndex('right')} width="96" height="121" viewBox="0 0 96 121" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M25.96 81.3458L33.04 88.4258L72.64 48.8258L33.04 9.22583L25.96 16.3058L58.48 48.8258L25.96 81.3458Z" fill="white"/>
+                            </svg>
+                        </div>
+
+                        <div className="index">
+                            {index + 1} / {images.length}
+                        </div>
                     </div>
                 )}
 
